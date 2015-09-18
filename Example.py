bae85d54862eaa7877aab1b14e89533547a06845
DmitriyLeybel <dmleybel@gmail.com>
2015-09-18 08:36:31 -0700

    Cleaned up code; made more pythonic. Added an example script(Example.py) which shows how to use IAC_Gangs.py

---
+++ Example.py
@@ -0,0 +1,8 @@
+from IAC_Gangs import *
+
+com = build() # Builds an object to contain the nodes and their respective features
+com.compose() # Composes the proper nodal properties and connections
+net = network(com.dic) # Creates a network object which can be probed, and activations, spread
+net.probe('Jets') # String argument as the property or instance to be probed
+net.activate(200) # Spreads the activation in the network for a select number of epochs
+net.check('Jets','Sharks') # Checks the final values of the desired nodes
--- IAC_Gangs.py
+++ IAC_Gangs.py
@@ -1,24 +1,21 @@
-g = open("ginfo.txt").read()
-lines = g.splitlines()
 
-cats = lines[0].split(' ')
-
-dProbe_input = .2
-ex_ia = .05
-in_ia = .03
-decay_rate = .05
-resting_value = .1
-m = -0.2
-M = 1.0
 
 
 class node():   # Will be the superclass of inode and pnode
+
+    ex_ia = .05
+    in_ia = .03
+    decay_rate = .05
+    m = -0.2
+    M = 1.0
+    resting_value = .1
+
     def __init__(self, name, category):
         self.name = name
         self.category = category
         self.connected_enodes = []
         self.connected_inodes = []
-        self.resting_value = resting_value
+        self.resting_value = node.resting_value
         self.probe_input=0
         self.input = 0
         self.effect = 0
@@ -30,12 +27,12 @@ class node():   # Will be the superclass of inode and pnode
         iInput = 0
         for node in self.connected_inodes:
             iInput -= node.effect
-        self.input = self.probe_input + (ex_ia*eInput) + (in_ia*iInput)
+        self.input = self.probe_input + (node.ex_ia*eInput) + (node.in_ia*iInput)
         if self.input > 0:
-            self.effect = (M-self.effect)*self.input
+            self.effect = (node.M-self.effect)*self.input
         else:
-            self.effect = (self.effect-m)*self.input
-        self.effect -= decay_rate*(self.effect-resting_value)
+            self.effect = (self.effect-node.m)*self.input
+        self.effect -= node.decay_rate*(self.effect-node.resting_value)
 
 
 class inode(node):
@@ -54,53 +51,69 @@ class pnode(node):
             elif isinstance(x, pnode):
                 self.connected_inodes.append(x)
 
-llines = [x.split(' ') for x in lines]
-dic = {}
-for x in llines[1:]: #Builds dictionary out of inode objects
-    dic[x[0]]=inode(x[0],cats[0])
-for x in llines[1:]: #Adds to dictionary the pnode objects
-    c= 1
-    for y in x[1:]:
-        dic[y]= pnode(y,cats[c])
-        c+=1
-for x in cats[1:]: #Connects the properties in the same category
-    samecatlist=[]
-    for y in dic:
-        if dic[y].category==x:
-            samecatlist.append(dic[y])
-    for y in dic:
-        if dic[y].category==x:
-            dic[y].connect(*samecatlist)
-for x in llines[1:]: #Connects the inodes to their respective pnodes
-    l = [dic[y] for y in x[1:]]
-    dic[x[0]].connected_enodes = dic[x[0]].connected_enodes + l
-for x in dic:  # Connects pnodes to their respective inodes
-    if isinstance(dic[x],inode):
-        for y in dic[x].connected_enodes:
-            y.connected_enodes.append(dic[x])
-inodelist = []
-for x in dic:   # Connects instances to the other instances
-    if isinstance(dic[x], inode):
-        inodelist.append(dic[x])
-for x in dic:
-    if isinstance(dic[x], inode):
-        dic[x].connected_inodes = inodelist
+
+class build():
+    g = open("ginfo.txt").read()
+    lines = g.splitlines()
+    cats = lines[0].split(' ')
+    dProbe_input = .2
+
+    def compose(self):
+
+        llines = [x.split(' ') for x in build.lines]
+        dic = {}
+        for x in llines[1:]: #Builds dictionary out of inode objects
+            dic[x[0]]=inode(x[0],build.cats[0])
+        for x in llines[1:]: #Adds to dictionary the pnode objects
+            c= 1
+            for y in x[1:]:
+                dic[y]= pnode(y,build.cats[c])
+                c+=1
+        for x in build.cats[1:]: #Connects the properties in the same category
+            samecatlist=[]
+            for y in dic:
+                if dic[y].category==x:
+                    samecatlist.append(dic[y])
+            for y in dic:
+                if dic[y].category==x:
+                    dic[y].connect(*samecatlist)
+        for x in llines[1:]: #Connects the inodes to their respective pnodes
+            l = [dic[y] for y in x[1:]]
+            dic[x[0]].connected_enodes = dic[x[0]].connected_enodes + l
+        for x in dic:  # Connects pnodes to their respective inodes
+            if isinstance(dic[x],inode):
+                for y in dic[x].connected_enodes:
+                    y.connected_enodes.append(dic[x])
+        inodelist = []
+        for x in dic:   # Connects instances to the other instances
+            if isinstance(dic[x], inode):
+                inodelist.append(dic[x])
+        for x in dic:
+            if isinstance(dic[x], inode):
+                dic[x].connected_inodes = inodelist
+        self.dic=dic
+
 
 class network():
-    def __init__(self):
-        self.previouslyActiveNode=None
 
-    def probe(self, nodes):  # Specifies the node(s) to be probed
-        for node in nodes:
-            node.probe_input = .2
+    def __init__(self, dic):
+        self.dic = dic
+
+    def probe(self, *strnodes):  # Specifies the node(s) to be probed
+        for strnode in strnodes:
+            self.dic[strnode].probe_input = .2
 
     def activate(self, epochs=200):  # Sends the activation through the network
         for x in range(0,epochs):
-            for node in dic:
-                dic[node].update_act()
+            for node in self.dic:
+                self.dic[node].update_act()
+
+    def check(self, *strnodes):  # Takes arguments as strings of instances or properties and outputs their respective effects
+        s = ''
+        for strnode in strnodes:
+            effect = self.dic[strnode].effect
+            binding = '{0}: {1} \n'.format(strnode,effect)
+            s = s + binding
+        print(s)
 
 
-net = network() # Create network object
-net.probe([dic['Sharks']]) # Probe node(s)
-net.activate(100) # Begin to update network | default epochs = 200
-print(dic['Phil'].effect, dic['Rick'].effect, dic['Doug'].effect )
