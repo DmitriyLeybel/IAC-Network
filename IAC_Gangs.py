26d379147cd475e119e3c54985647141a079cea6
DmitriyLeybel <dmleybel@gmail.com>
2015-09-24 20:48:43 -0700

    Debugging tools included, and instance node and name property node equivalence fixed. Update buffer added.

--- IAC_Gangs.py
+++ IAC_Gangs.py
@@ -16,38 +16,43 @@ class node():   # Will be the superclass of inode and pnode
         self.probe_input=0
         self.input = 0
         self.effect = 0
+        self.buffered_effect = 0
 
     def update_act(self):
+        self.effect=0
+        self.input=0
         eInput = 0
+
         for node in self.connected_enodes:
-            eInput += node.effect
+            eInput += node.buffered_effect
+
         iInput = 0
+
         for node in self.connected_inodes:
-            iInput += node.effect
+            iInput += node.buffered_effect
+
         self.input = self.probe_input + (node.ex_ia*eInput) - (node.in_ia*iInput)
 
+        # if self.name == 'node0':  ####DEBUGING
+        #    print('eInput:{0} || iInput: {1}'.format(eInput,iInput))
         if self.input > 0:
             self.effect = (node.M-self.effect)*self.input
-        elif self.input < 0:
+
+        elif self.input <= 0:
             self.effect = (self.effect-node.m)*self.input
+        # if self.name == 'node0':  ####DEBUGING
+        #    print(self.effect)
         self.effect -= node.decay_rate*(self.effect-node.resting_value)
 
 
 class inode(node):
 
-    def connect(self, *other):
-        for x in other:
-            self.connected_pnodes.append(x)
+    pass
 
 
 class pnode(node):
 
-    def connect(self, *other):
-        for x in other:
-            if isinstance(x, inode):
-                self.connected_enodes.append(x)
-            elif isinstance(x, pnode):
-                self.connected_inodes.append(x)
+    pass
 
 
 class build():  # Used to create the nodal structure
@@ -59,49 +64,34 @@ class build():  # Used to create the nodal structure
 
         llines = [x.split(' ') for x in build.lines]
         dic = {}
-        for x in llines[1:]: #Builds dictionary out of inode objects
-            dic[x[0]]=inode(x[0],build.cats[0])
-        for x in llines[1:]: #Adds to dictionary the pnode objects
-            c= 1
-            for y in x[1:]:
-                dic[y]= pnode(y,build.cats[c])
-                c+=1
-        for x in build.cats[1:]: #Connects the properties in the same category
-            samecatlist=[]
-            for y in dic:
-                if dic[y].category==x:
-                    samecatlist.append(dic[y])
-            for y in dic:
-                if dic[y].category==x:
-                    dic[y].connect(*samecatlist)
-        for x in llines[1:]: #Connects the inodes to their respective pnodes
-            l = [dic[y] for y in x[1:]]
-            dic[x[0]].connected_enodes = dic[x[0]].connected_enodes + l
-        for x in dic:  # Connects pnodes to their respective inodes
-            if isinstance(dic[x],inode):
-                for y in dic[x].connected_enodes:
-                    y.connected_enodes.append(dic[x])
+        for x in llines[1:]:    # Builds property nodes and adds them to dictionary
+            for y in range(len(build.cats)):
+                dic[x[y]] = pnode(x[y],build.cats[y])
 
-        inodelist = []
+        for x,y in zip(llines[1:],range(len(llines)-1)):    # Builds instance nodes, connects them to property nodes
+            dic['node{0}'.format(y)] = inode('node{0}'.format(y),'node')
+            for a in range(len(build.cats)):
+                dic['node{0}'.format(y)].connected_enodes.append(dic[x[a]])
+                dic[x[a]].connected_enodes.append(dic['node{0}'.format(y)]) # Also populates connected_enodes of property nodes
 
-        for x in dic:   # Connects instances to the other instances
-            if isinstance(dic[x], inode):
-                inodelist.append(dic[x])
+        ilist = []
+        for x in dic:   # Populates the connected_inodes of the instance nodes
+            if dic[x].category == 'node':
+                ilist.append(dic[x])
         for x in dic:
-            if isinstance(dic[x], inode):
-                dic[x].connected_inodes = inodelist
+            if dic[x].category == 'node':
+                for y in ilist:
+                    if dic[x].name != y.name:
+                        dic[x].connected_inodes.append(y)
 
-        for x in dic:   # Removes pnodes from their respective inode and enode lists
-            if type(dic[x]) == pnode:
-                dic[x].connected_inodes.remove(dic[x])
-            f = []
-            if type(dic[x]) == inode:
+
+        for x in dic:    # Populates property nodes' connected_inodes
+            if dic[x].category in build.cats:
                 for y in dic:
-                    if type(dic[y]) == inode:
-                        if dic[x] != dic[y]:
-                            f.append(dic[y])
-                dic[x].connected_inodes = f
-        self.dic=dic
+                    if dic[y].category == dic[x].category and dic[x] != dic[y]:
+                        dic[x].connected_inodes.append(dic[y])
+
+        self.dic = dic
 
 
 class network():
@@ -112,11 +102,17 @@ class network():
     def probe(self, *strnodes):  # Specifies the node(s) to be probed
         for strnode in strnodes:
             self.dic[strnode].probe_input = .2
+            self.dic[strnode].buffered_effect = .2
 
-    def update(self, iterations=200):  # Sends the activation through the network
+    def update(self, iterations=200, debug=0, dname=''):  # Sends the activation through the network
         for x in range(0,iterations):
             for node in self.dic:
                 self.dic[node].update_act()
+                if debug == 1:
+                    if self.dic[node].name == dname:
+                        print(self.dic[node].effect)
+            for node in self.dic:
+                self.dic[node].buffered_effect = self.dic[node].effect
 
     def check(self, *strnodes):  # Takes arguments as strings of instances or properties and outputs their respective effects
         s = ''
