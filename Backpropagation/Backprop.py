775e03729440478ae2bf46737e9196d9c4cd6321
DmitriyLeybel <dmleybel@gmail.com>
2015-11-21 20:32:56 -0800

    Added a method to train sets, and another to compute the RMSE. Included an example of XOR.

--- Backpropagation/Backprop.py
+++ Backpropagation/Backprop.py
@@ -17,10 +17,14 @@ class Network:
         self.outputBias = np.random.rand(sizeOutput)
 
         self.target = []
+        self.targetSetList = []
         # Used for backpropagation of error
         self.inputOfOutput = np.empty(sizeOutput)
         self.hiddenToOutput = np.empty(sizeHidden)
         self.outputErrInfo = []
+
+        self.errorLog = []
+        self.errorLogList = []
 
     def activationFunction(self,val):
         return 1/(1 + np.exp(-val))
@@ -53,7 +57,7 @@ class Network:
         self.outputErrInfo = (self.target - self.output) *aFD(self.inputOfOutput)
         self.hiddenOutputWeightCorrection = self.learningRate*(np.outer(self.hiddenToOutput,self.outputErrInfo))
         self.outputBiasCorrection = self.learningRate*self.outputErrInfo
-        self.hiddenErrCorrSums = np.dot(self.outputErrInfo, self.hiddenOutput)
+        self.hiddenErrCorrSums = np.inner(self.outputErrInfo, self.hiddenOutput) # Changed from dot to inner
         self.hiddenErrInfo = self.hiddenErrCorrSums*aFD(self.inputToHidden)
         self.inputHiddenWeightCorrection = self.learningRate*np.outer(self.input, self.hiddenErrInfo)
         self.hiddenBiasCorrection = self.learningRate*self.hiddenErrInfo
@@ -64,20 +68,42 @@ class Network:
         self.inputHidden += self.inputHiddenWeightCorrection
         self.hiddenBias += self.hiddenBiasCorrection
 
+    def errorCalc(self):
+        error = np.sqrt(sum(np.square(self.target-self.output))/len(self.target))
+        return error
+
     def fire(self,cycles):
         for x in range(cycles):
             self.feedForward()
             self.feedBack()
             self.update()
+            self.errorLog.append(self.errorCalc())
         self.feedForward()
         return self.output
 
+    def trainSets(self, listOfSets, cycles):
+        # Every set will be composed of a tuple, with the first value being the input and the seoond, the target.
+        for Set in listOfSets:
+            self.assignInput(Set[0])
+            self.assignTarget(Set[1])
+            # Resets the error log, so you can reuse the variable
+            self.errorLog = []
+            n.fire(cycles)
+            self.errorLogList.append(self.errorLog)
+
 
 if __name__ == '__main__':
-    n = Network(5,5,5,.7)
-    n.assignInput([1,0,1,1,0])
-    n.assignTarget([1,0,1,0,0])
-    print(n.fire(10))
+    n = Network(2,1,2,.9)
+    n.trainSets([([0,0],[0]),([0,1],[1]),([1,0],[1]),([1,1],[0])],1000)
+    # Prints the errors of the sets during training
+    # for set,x in zip(n.errorLogList,range(4)):
+    #     print('Set {0} Error List:{1}'.format(x+1,set))
 
-
-
+    n.assignInput([0,0])
+    n.assignTarget([0])
+    n.feedForward()
+    print(n.errorCalc())
+    # n.assignInput([1,0,1,1,0])
+    # n.assignTarget([1,0,1,0,0])
+    # print(n.fire(20))
+    # print(n.errorCalc())
